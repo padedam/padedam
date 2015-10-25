d5194e2887ba728673742d39682aa4e35650cfd5
ZygimantasKoncius <konciuszygimantas@gmail.com>
2015-10-25 15:57:14 +0200

    deleted apie bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new NFQ\HomeBundle\NFQHomeBundle(),
-            new NFQ\ApieBundle\NFQApieBundle(),
             new NFQ\FrontendBundle\NFQFrontendBundle(),
             new NFQ\AssistanceBundle\NFQAssistanceBundle(),
             );
