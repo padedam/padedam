d15144b6c14bc4c291b3101491b34c34ca683232
Renaldas Zajanckauskas <renaldas@zajanckauskas.lt>
2015-10-27 16:55:18 +0200

    forms themes

--- src/NFQ/UserBundle/Form/RegistrationType.php
+++ src/NFQ/UserBundle/Form/RegistrationType.php
@@ -9,41 +9,46 @@ namespace NFQ\UserBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-class RegistrationType extends AbstractType{
+
+class RegistrationType extends AbstractType
+{
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->remove('username');
         $builder->add('first_name', 'text',
             array(
-                "label"=>"user.label_name",
-                "required"=>true,
+                "label" => "user.label_name",
+                "required" => true,
             )
         );
         $builder->add('last_name', 'text',
             array(
-                "label"=>"user.label_surname",
-                "required"=>true,
+                "label" => "user.label_surname",
+                "required" => true,
             )
         );
         $builder->add('phone', 'text',
             array(
-                "label"=>"user.label.phone",
-                "required"=>true,
+                "label" => "user.label.phone",
+                "required" => true,
             )
         );
         $builder->add('birthday', 'date',
             array(
-                "label"=>"user.birth_year",
-                "required"=>true,
+                "label" => "user.birth_year",
+                "required" => true,
             )
         );
         $builder->add('description', 'textarea',
             array(
-                "label"=>"user.about_me",
-                "required"=>true,
+                'label' => "user.about_me",
+                'required' => true,
+                'attr' => [
+                    'data-help' => 'user.short_about_me_desc',
+                ]
             )
         );
-        //$builder->add('myFieldName', 'text', array('label_format' => 'My Help Message'));
+        /*$builder->add('field', 'text', ['label' => 'user.short_about_me_desc']);*/
     }
 
     public function getParent()
