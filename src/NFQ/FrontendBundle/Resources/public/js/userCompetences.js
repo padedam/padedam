b6d8d9dca0e0dfcb55835a59faf41d7d4330732e
Renaldas Zajanckauskas <renaldas@zajanckauskas.lt>
2015-11-20 12:50:36 +0200

    assistance field is now select2 and is resetting on value selected, when parent tag is removed it appears as an option in new competence selections

--- src/NFQ/FrontendBundle/Resources/public/js/userCompetences.js
+++ src/NFQ/FrontendBundle/Resources/public/js/userCompetences.js
@@ -1,158 +1,81 @@
-$(function(){
-    var competences = function (element) {
-        var taggable = element;
-        taggable.val('narsieji');
-        taggable.select2({
+var competences = function (element) {
+    var taggable = element;
+    taggable.val('narsieji');
+    taggable.select2({
         initSelection: function (element, callback) {
-                var obj_id = element.val();
-                if (obj_id !== "")  {
-                    $.getJSON(
-                        taggable.data('current'),
-                        {obj_id: obj_id, parent_id: taggable.data('parentid')},
-                        function (response) {
-                            if (response.status == 'success') {
-                                element.val(JSON.stringify(response.tags));
-                                callback(response.tags);
-                            } else {
-                                // alert(response.message);
-                            }
-                        });
-                }
-            },
-            triggerChange: true,
-            allowClear: true,
-            quietMillis: 250,
-            minimumInputLength: 2,
-            tags: true,
-            tokenSeparators: [',', ';'],
-            createSearchChoice: function (term) {
+            var obj_id = element.val();
+            if (obj_id !== "")  {
+                $.getJSON(
+                    taggable.data('current'),
+                    {obj_id: obj_id, parent_id: taggable.data('parentid')},
+                    function (response) {
+                        if (response.status == 'success') {
+                            element.val(JSON.stringify(response.tags));
+                            callback(response.tags);
+                        } else {
+                            // alert(response.message);
+                        }
+                    });
+            }
+        },
+        triggerChange: true,
+        allowClear: true,
+        quietMillis: 250,
+        minimumInputLength: 2,
+        tags: true,
+        tokenSeparators: [',', ';'],
+        createSearchChoice: function (term) {
+            return {
+                id: $.trim(term),
+                text: $.trim(term) + ' (Naujas)'
+            };
+        },
+        ajax: {
+            url: taggable.data('match'),
+            dataType: 'json',
+            data: function (term, page) {
                 return {
-                    id: $.trim(term),
-                    text: $.trim(term) + ' (Naujas)'
-                };
-            },
-            ajax: {
-                url: taggable.data('match'),
-                dataType: 'json',
-                data: function (term, page) {
-                    return {
-                        tag: term,
-                        parent_id: taggable.data('parentid')
-                    }
-                },
-                results: function (data, page) {
-                    if(data.status == 'success') {
-                        return {
-                            results: data.tags
-                        };
-                    }
+                    tag: term,
+                    parent_id: taggable.data('parentid')
                 }
             },
-            formatSelectionTooBig: function (limit) {
-                return "Daugiausiai raktažodžių galite parinkti " + limit;
+            results: function (data, page) {
+                if(data.status == 'success') {
+                    return {
+                        results: data.tags
+                    };
+                }
             }
-        }).on('select2-removed', function (e) {
-            //send delete tag request
-            remTag(taggable.data('rem'), {tag: e.choice});
-        }).on('select2-selecting', function (e) {
-            var _data = e.choice;
-            taggable.val(JSON.stringify(_data));
-            saveTag(taggable.data('save'), e.choice, taggable.data('parentid'));
-        });
-    };
+        },
+        formatSelectionTooBig: function (limit) {
+            return "Daugiausiai raktažodžių galite parinkti " + limit;
+        }
+    }).on('select2-removed', function (e) {
+        //send delete tag request
+        $.post(
+            taggable.data('rem'),
+            {tag: e.choice},
+            function (response) {},
+            'json'
+        );
 
-    /** SELECT ALL ELEMENTS AND MAKE THEM SELECT2 **/
-    $( ".form-select" ).each(function() {
-        var id = $(this).attr('id');
-        var element = $("#" + id);
-        competences(element);
+
+    }).on('select2-selecting', function (e) {
+        var _data = e.choice;
+        taggable.val(JSON.stringify(_data));
+        $.post(
+            taggable.data('save'),
+            {tag: e.choice, parent_id: taggable.data('parentid')},
+            function(response){},
+            'json'
+        );
     });
-
-
-/* create a new competence category */
-    $("#new_competence").change(function(e){
-        e.preventDefault();
-        var selval = $("#new_competence").val();
-        var seltext = $("#new_competence option:selected" ).text();
-
-        var input = $('<input/>', {
-            'class': 'form-control',
-            'id': "child_"+selval,
-            'data-save': $("#new_competence").data('save'),
-            'data-current': $("#new_competence").data('current'),
-            'data-rem' :$("#new_competence").data('rem'),
-            'data-match': $("#new_competence").data('match'),
-            'data-parentid': selval
-        });
-
-        var formgroup = $('<div/>', {
-            'class' : 'form-group'
-        });
-
-        var label = $( '<label>', {
-            'class': 'col-sm-2 control-label required',
-            'text': seltext
-        });
-        var col9 = $( '<div/>', {
-            'class': 'col-sm-9'
-        });
-        var col1 = $( '<div/>', {
-            'class': 'col-sm-1'
-        });
-        var remlink = $('<a/>', {
-            'class': 'rem-cat',
-            'data-id': selval,
-            'data-text': seltext,
-            'data-rem': $("#new_competence").data('rem'),
-            'text': 'pašalinti',
-            'href': '#'
-        });
-
-        //add element to DOM
-        $('#wrapper').prepend( formgroup.append( label).append( col9.append( input )).append( col1.append( remlink) ));
-
-        //make element select2
-        var element = $("#" + "child_"+selval);
-        competences(element);
-        //remove selected element from select list and reset selection
-        $("#new_competence option[value="+selval+"]").remove().prop('selectedIndex',0);
-        saveTag($("#new_competence").data('save'), {id:selval, text:seltext}, null);
-
-        //bind remove category button
-        bindRemcat();
-    });
-
-    bindRemcat();
-
-});
-
-function saveTag(url, tag, parent){
-    $.post(
-        url,
-        {tag: tag, parent_id: parent},
-        function(response){
-            return response;
-        },'json'
-    );
-}
-
-function remTag(url, data, parent){
-    $.post(
-        url,
-        data,
-        function (response) {
-            if(parent === true && response.status == 'success'){
-                $()
-            }
-        },'json'
-    );
-}
+};
 
 var bindRemcat = function(){
     $(".rem-cat").unbind('click').bind('click', function(e){
         e.preventDefault();
         var el = $(this);
-        var id =el.data('id');
         //send delete tag request
         $.post(
             el.data('rem'),
@@ -160,8 +83,101 @@ var bindRemcat = function(){
             function (response) {
                 if(response.status == 'success'){
                     el.parents('.form-group').hide('slow');
+                    //add removed element to select list
+                    $('#new_competence').append($('<option />', { value : el.data('id'), text: el.data('text') }));
+                    $('#new_competence').select2("destroy").select2().on('select2-selecting', function (e) {
+                        var _data = e.choice;
+                        createParentElement(e);
+                    });
                 }
             },'json'
         );
     });
 }
+
+
+/* create a new competence category */
+function createParentElement (e){
+    var selval = e.choice.id;
+    var seltext = e.choice.text;
+
+    if(selval == 0) {
+        throw new Error("Selected default value");
+    }
+    $.post(
+        $("#new_competence").data('save'),
+        {tag: {id: selval, text: seltext}, parent_id: null},
+        function(response){
+            if(response.status == 'success'){
+                //add element to DOM
+                var input = $('<input/>', {
+                    'class': 'form-control',
+                    'id': "child_"+selval,
+                    'data-save': $("#new_competence").data('save'),
+                    'data-current': $("#new_competence").data('current'),
+                    'data-rem' :$("#new_competence").data('rem'),
+                    'data-match': $("#new_competence").data('match'),
+                    'data-parentid': selval
+                });
+
+                var formgroup = $('<div/>', {
+                    'class' : 'form-group'
+                });
+
+                var label = $( '<label>', {
+                    'class': 'col-sm-2 control-label required',
+                    'text': seltext
+                });
+                var col9 = $( '<div/>', {
+                    'class': 'col-sm-9'
+                });
+                var col1 = $( '<div/>', {
+                    'class': 'col-sm-1'
+                });
+                var remlink = $('<a/>', {
+                    'class': 'rem-cat',
+                    'data-id': selval,
+                    'data-text': seltext,
+                    'data-rem': $("#new_competence").data('rem'),
+                    'text': 'pašalinti',
+                    'href': '#'
+                });
+
+                $('#wrapper').prepend( formgroup.append( label).append( col9.append( input )).append( col1.append( remlink) ));
+
+                //make element select2
+                var element = $("#" + "child_"+selval);
+                competences(element);
+
+                //remove selected element from select list and reset selection
+                $("#new_competence option[value="+selval+"]").remove();
+                $("#new_competence").select2("destroy").select2().on('select2-selecting', function (e) {
+                    var _data = e.choice;
+                    createParentElement(e);
+                });
+                //enable remove category button
+                bindRemcat();
+            }
+        },'json'
+    );
+}
+
+$(function(){
+    /** SELECT ALL ELEMENTS AND MAKE THEM SELECT2 **/
+    $( ".form-select" ).each(function() {
+        var id = $(this).attr('id');
+        var element = $("#" + id);
+        competences(element);
+    });
+
+    /** make #new_competence select 2 **/
+    var new_competence = $("#new_competence").select2({
+        placeholder: "Pasirinkite sritį"
+    }).on('select2-selecting', function (e) {
+        var _data = e.choice;
+        createParentElement(e);
+    });
+
+    bindRemcat();
+
+});
