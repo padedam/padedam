fe5e61c14673070f101f4d8b5173dd16df6caf01
matas_valuzis <matas.valuzis@gmail.com>
2015-11-21 10:24:39 +0200

    added mising file

---
+++ src/NFQ/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matas
+ * Date: 2015.11.19
+ * Time: 15:23
+ */
+namespace NFQ\UserBundle\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use FOS\UserBundle\Controller\SecurityController as BaseController;
+use Symfony\Component\Security\Core\SecurityContext;
+
+class SecurityController extends BaseController{
+
+    public function loginAction(){
+        $request = $this->container->get('request');
+
+        $ajax = $request->query->get('ajax', false);
+        if ($ajax === false){
+            $response = parent::loginAction();
+            return $response;
+        }
+
+        /* @var $request \Symfony\Component\HttpFoundation\Request */
+        $session = $request->getSession();
+        /* @var $session \Symfony\Component\HttpFoundation\Session\Session */
+
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        if ($error) {
+            // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
+            $error = $error->getMessage();
+        }
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
+
+        $csrfToken = $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate');
+
+        $response = new JsonResponse();
+        $response->setData(array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+            'csrf_token' =>  $csrfToken,
+        ));
+
+        return $response;
+    }
+    public function modal_loginAction(){
+        $csrfToken = $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate');
+
+        return $this->container->get('templating')->renderResponse('NFQUserBundle:Security:modal_login.html.twig', ['csrf_token' => $csrfToken]);
+    }
+}
